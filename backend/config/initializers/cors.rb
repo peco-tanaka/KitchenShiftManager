42486d7399eced3a6db43b5ad334312e40925045
peco-tanaka <ok.computeruk1929@gmail.com>
2025-07-02 14:32:22 +0900

    fix: Rubocopの実行

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -2,11 +2,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # 開発環境用設定
-    origins Rails.env.development? ? ["http://localhost:5173", "http://127.0.0.1:5173"] : ENV.fetch("FRONTEND_ORIGIN", "")
+    origins Rails.env.development? ? [ "http://localhost:5173", "http://127.0.0.1:5173" ] : ENV.fetch("FRONTEND_ORIGIN", "")
 
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
       credentials: true  # Session Cookie認証のため必要
   end
 end
