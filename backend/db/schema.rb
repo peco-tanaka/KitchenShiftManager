6f7690ee38379551c6b055271cfc583cc2739e3c
peco-tanaka <ok.computeruk1929@gmail.com>
2025-07-03 19:05:51 +0900

    feat: Userモデルの作成
    # 1. models/user.rb
      - enum追加
      - バリデーション追加
      - 認証キーの設定
    
    # 2. マイグレーションの実行
      - "last_name", "first_name"カラムを追加

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_03_053019) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_03_092803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_03_053019) do
     t.date "terminated_on", comment: "退職日"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "last_name", limit: 50, null: false
+    t.string "first_name", limit: 50, null: false
     t.index ["employee_number"], name: "index_users_on_employee_number", unique: true
     t.index ["role"], name: "index_users_on_role"
   end
